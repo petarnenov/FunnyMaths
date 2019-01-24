c85cbeee77615c4d7d373e6d2b3dbd6a151118aa
Petar Nenov <petar_nenov@abv.bg>
2019-01-24 23:33:32 +0200

    add dockerfile

--- FnnyMath/newints.ts
+++
@@ -1,9 +0,0 @@
-export class Animal{
-    constructor(private name: string){}
-    info(): void{
-        console.log("Name of the Animal is:"+this.name);
-    }
-}
-
-let animal = new Animal("Greg");
-animal.info();
