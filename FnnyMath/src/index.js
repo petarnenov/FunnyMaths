96b7053bc9d1dc82ed2f0238d531d5811252f378
Petar Nenov <petar.nenov-petrov@geowealth.com>
2024-10-08 19:51:20 +0300

    fix: errors

---
+++ FnnyMath/src/index.js
@@ -0,0 +1,22 @@
+import Examine from './Examine';
+function res() {
+    let addTag = document.getElementById('exercises');
+    if (!addTag) {
+        return;
+    }
+    while (addTag.hasChildNodes()) {
+        if (addTag.firstChild) {
+            addTag.removeChild(addTag.firstChild);
+        }
+    }
+    Examine.badAnswer = 0;
+    Examine.goodJob = 0;
+    Examine.showResults();
+    for (let i = 0; i < 10; i++) {
+        let examine = new Examine();
+        examine.generateExercise();
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    res();
+});
